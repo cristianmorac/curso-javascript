d36ef8ec2d2012636c794372555157fe296afdce
Cristian Mora <cristianfmorac@gmail.com>
2023-03-16 18:09:08 -0500

    Curso JavaScript variables

---
+++ variables/variables.js
@@ -0,0 +1,27 @@
+/* Variables */
+var nombre = "Cristian";
+var edad = 30
+var mensaje_final = "Lorraine"+" y "+nombre
+/* Ver cuadro de texto */
+alert(mensaje_final);
+var preguntas = prompt("Escriba algo");
+/* Ver en consola */
+console.log(mensaje_final)
+
+/* Operaciones aritmeticas */
+var numero = 100;
+var numero2 = 200;
+var numero = numero + 10 -5 * 10 / 20;
+var peso = 79
+var estatura = 1.72
+var IMC = peso / estatura
+console.log(IMC)
+console.log(numero2)
+numero2 +=10;
+console.log(numero2)
+numero2 *=10;
+console.log(numero2)
+numero2 -=2;
+console.log(numero2)
+numero2 /= 2;
+console.log(numero2)
